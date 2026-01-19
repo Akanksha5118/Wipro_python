82f5248ec2aeff4373fb40967207d49a659991c6
Akanksha Kumari <ak97531kumari@gmail.com>
2026-01-19 10:55:56 +0530

    Added day 5 Inheritance

--- Day_5/TC_Multiinheri.py
+++ Day_5/TC_Multiinheri.py
@@ -1,13 +1,17 @@
+class A:
+    def showA(self):
+        print("A from class A")
+
 class B:
     def showB(self):
         print("B")
 
-def showA(self):
-    print("A from class B")
-
-class C(B,A):
+class C(A, B):
     pass
 
-c.C()
+c = C()
 c.showA()
 c.showB()
+
+
+
